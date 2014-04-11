5a2f462359deccf4e740acfc1ee7ff083b4fccf4
Andrew <ndrewniklas@gmail.com>
2014-04-11 15:32:48 -0700

    Signed-off-by: Andrew <ndrewniklas@gmail.com>

--- ProgAssgmnt1/src/edu/csupomona/cs/cs141/progassgmnt1/User.java
+++ ProgAssgmnt1/src/edu/csupomona/cs/cs141/progassgmnt1/User.java
@@ -1,6 +1,15 @@
 /**
- * 
+ * CS 141: Introduction to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Assignment #N
+ *
+ * <description-of-assignment>
+ *
+ * Team #N / (or name if individual)
+ *   Ben Nickerson, Andrew Nipp, Dylan Nguyen, Kurt Newcomb, Andrew Niklas 
  */
+
 package edu.csupomona.cs.cs141.progassgmnt1;
 
 /**
