152de4c4f638d0053cc17e331c2e0952a9254f9a
amnipp <Andrew@134.71.114.108>
2014-04-13 19:31:44 -0700

    Test commit with project description and the Enemy class

---
+++ ProgAssgmnt1/src/edu/csupomona/cs/cs141/progassgmnt1/Enemy.java
@@ -0,0 +1,26 @@
+/**
+ * CS 141: Introduction to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ * 
+ * Programming Assignment 1
+ * 
+ * A team assignment to create skeleton code for a First person shooter video game
+ * 
+ * Team Ba^2kd
+ * Ben Nickerson (Captain)
+ * Kurt Newcomb
+ * Dylan Nguyen
+ * Andrew Niklas
+ * Andrew Nipp
+ */
+package edu.csupomona.cs.cs141.progassgmnt1;
+
+/**
+ * This class defines the Enemy NPC for the First Person Shooter
+ * 
+ * @author Andrew Nipp
+ *
+ */
+public class Enemy {
+
+}
